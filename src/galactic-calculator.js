f7c71fcecfd6cf2a8834438463ffd3913bb351c3
Nanette Girzi <ngirzi@gmail.com>
2018-03-16 14:11:40 -0700

    add new logic for calculateAge to include age in seconds

--- src/galactic-calculator.js
+++ src/galactic-calculator.js
@@ -8,13 +8,18 @@ export class Age {
   }
 
   calculateAge() {
-    let today = new Date();
-    let birthday = new Date (this.birthday);
-    let age = today.getFullYear() - birthday.getFullYear();
-    let month = today.getMonth() - birthday.getMonth();
-    if (month < 0 || (month === 0 && today.getDate() < birthday.getDate())){
-      age--;
-    }
-    return age;
+    let birthday = this.birthday;
+    let year = birthday.slice(0,4);
+    let month = birthday.slice(5,7);
+    let day = birthday.slice(8,10);
+    let date = month + '/' + day + '/' + year + ' 00:00:00';
+    let newDate = new Date(date);
+    let millisBirthday = newDate.getTime();
+    let millisNow = Date.now();
+    let ageInSeconds = (millisNow - millisBirthday)/1000;
+    let ageInYears = Math.floor(ageInSeconds/31536000);
+    return ageInYears;
   }
+
+
 }
